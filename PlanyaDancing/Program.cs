18e07f242d6dae8de0d32d838384aff6fca8a809
Maxim <prefectsol@mail.ru>
2021-11-05 15:48:23 +0500

    version 2.0

--- PlanyaDancing/Program.cs
+++ PlanyaDancing/Program.cs
@@ -9,29 +9,34 @@ namespace PlanyaDancing
         {
             Frames instance = new Frames();
 
+            var frame = instance.frames;
+
             int consoleWidth = 45;
             int consoleHeight = 50;
 
-#pragma warning disable CA1416 // Checking platform compatibility
-            Console.SetWindowSize(width: consoleWidth,
-                                  height: consoleHeight);
             int speedFrame = 80;
-            var frame = instance.frames;
+            int countFrames = instance.frames.Length;
 
+#pragma warning disable CA1416 // Проверка совместимости платформы
+            Console.SetWindowSize(consoleWidth, consoleHeight);
+
+            Animation(countFrames, frame, speedFrame);
+        }
+        private static void Animation(int countFrames, string[] frame, int speedFrame)
+        {
             while (true)
             {
-                int n = 11;
-                for (int i = 0; i < n; i++) 
+                int n = countFrames;
+                for (int i = 0; i < n; i++)
                 {
-                    Console.Write(frame[i]);
+                    Console.Write("\r" + frame[i]);
                     Thread.Sleep(speedFrame);
                 }
                 n = 0;
-                for (int i = 10; i > n; i--) 
+                for (int i = countFrames - 1; i > n; i--, n++)
                 {
-                    Console.Write(frame[i]);
+                    Console.Write("\r" + frame[i]);
                     Thread.Sleep(speedFrame);
-                    n++;
                 }
             }
         }
